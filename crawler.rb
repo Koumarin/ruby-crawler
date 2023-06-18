fbacfe5dd214ba9e768161fa5cfdd5882752c935
Koumarin <alicefigueiredo837@gmail.com>
2023-06-18 16:10:38 -0300

    add recursive crawl on found links

--- crawler.rb
+++ crawler.rb
@@ -3,6 +3,11 @@ require 'net/http'
 require 'nokogiri'
 require 'optparse'
 
+def links_from_html(html)
+  doc = Nokogiri::HTML html
+  (doc.xpath '//a').map {|link| href = link['href']}
+end
+
 parser = OptionParser.new
 
 args = parser.parse!
@@ -18,11 +23,23 @@ visited   = []
 while unvisited.length > 0
   uri = unvisited.pop
 
+  puts "Visiting #{uri}"
+
   visited.push uri
 
   html = Net::HTTP.get uri
-  doc  = Nokogiri::HTML html
-  (doc.xpath '//a').each do |link|
-    puts link['href']
+
+  (links_from_html html).each do |link|
+    next if not link.start_with? '/'   # Skip non-root based links for now.
+
+    new_uri = URI link
+
+    if not new_uri.kind_of? URI::HTTP
+      new_uri = URI uri.origin + link
+    end
+
+    if not (visited + unvisited).include? new_uri
+      unvisited.push new_uri
+    end
   end
 end
