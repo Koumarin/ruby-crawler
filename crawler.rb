209e785c13867e1e52a449297c07cf063a43f7be
Koumarin <alicefigueiredo837@gmail.com>
2023-06-18 15:31:28 -0300

    add href extraction

--- crawler.rb
+++ crawler.rb
@@ -1,16 +1,28 @@
 #!/usr/bin/env ruby
 require 'net/http'
+require 'nokogiri'
 require 'optparse'
 
 parser = OptionParser.new
 
 args = parser.parse!
 
-if args.length != 1
-  puts 'Wrong number of arguments.'
+if args.length < 1
+  puts 'Please provide at least one URI to start crawling.'
   exit
 end
 
-path = args.first
+unvisited = args.map {|src| URI src}
+visited   = []
 
-puts Net::HTTP.get URI path
+while unvisited.length > 0
+  uri = unvisited.pop
+
+  visited.push uri
+
+  html = Net::HTTP.get uri
+  doc  = Nokogiri::HTML html
+  (doc.xpath '//a').each do |link|
+    puts link['href']
+  end
+end
